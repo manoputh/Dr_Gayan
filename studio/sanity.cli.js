b496bec58dbb7e7d7823df3099679638904716c8
manoputh <e21292@eng.pdn.ac.lk>
2026-02-14 12:11:42 +0530

    refactor: update sanity configuration to use environment variables for projectId and dataset

--- studio/sanity.cli.js
+++ studio/sanity.cli.js
@@ -1,13 +1,13 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from "sanity/cli";
 
 export default defineCliConfig({
-  api: {
-    projectId: '01snurzm',
-    dataset: 'production'
-  },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
-})
+   api: {
+      projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+      dataset: process.env.SANITY_STUDIO_DATASET,
+   },
+   /**
+    * Enable auto-updates for studios.
+    * Learn more at https://www.sanity.io/docs/cli#auto-updates
+    */
+   autoUpdates: true,
+});
--- studio/sanity.config.js
+++ studio/sanity.config.js
@@ -1,18 +1,18 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
 
 export default defineConfig({
-  name: 'default',
-  title: 'Dr_Gayan',
+   name: "default",
+   title: "Dr_Gayan",
 
-  projectId: '01snurzm',
-  dataset: 'production',
+   projectId: process.env.SANITY_STUDIO_PROJECT_ID,
+   dataset: process.env.SANITY_STUDIO_DATASET,
 
-  plugins: [structureTool(), visionTool()],
+   plugins: [structureTool(), visionTool()],
 
-  schema: {
-    types: schemaTypes,
-  },
-})
+   schema: {
+      types: schemaTypes,
+   },
+});
