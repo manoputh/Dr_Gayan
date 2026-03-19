b0aa43ed27c0f6e38ec93269fb5c667f659c3381
manoputh <e21292@eng.pdn.ac.lk>
2026-03-19 20:08:46 +0530

    refactor: remove unused schema files and update schema exports

--- studio/schemas/index.js
+++ studio/schemas/index.js
@@ -1,11 +1,7 @@
 // Import all schema files
 import siteSettings from "./siteSettings";
-import hero from "./hero";
-import service from "./service";
 import blogPost from "./blogPost";
 import category from "./category";
-import about from "./about";
-import program from "./program";
 
 // Export all schemas
-export const schemaTypes = [siteSettings, hero, service, blogPost, category, about, program];
+export const schemaTypes = [siteSettings, blogPost, category];
